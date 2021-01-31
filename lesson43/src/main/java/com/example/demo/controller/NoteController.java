515a2c0f5b52285f9e8cf12620f7514fe7a4577f
pas-monah <paspaspas999@gmail.com>
2021-02-01 00:24:45 +0200

    Редактирование заметок

--- lesson43/src/main/java/com/example/demo/controller/NoteController.java
+++ lesson43/src/main/java/com/example/demo/controller/NoteController.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.example.demo.model.Note;
 
 @Controller
@@ -35,6 +35,34 @@ public class NoteController {
 				.orElseGet(()->new Note())
 				.getId()+1);
 		notes.add(note);
+		return "redirect:/notes/all";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable(name="id") int id) {
+		notes.removeIf(n->n.getId()==id);
+		return "redirect:/notes/all";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable(name = "id") int id, Model model) {
+		model.addAttribute("note", notes.stream().filter(n -> n.getId() == id).findFirst().get());
+		return "note_update";
+	}
+	
+	@PostMapping("/note_update")
+	public String userUpdate(@ModelAttribute(name="note") Note note) {
+		int index = 0;
+		for(int i = 1; i<notes.size(); i++) {
+			if(notes.get(i).getId()==note.getId()) {
+				index = i;
+				break;
+			}
+		}
+		if(notes.get(index).getId()==note.getId()) {
+			notes.get(index).setLabel(note.getLabel());;
+			notes.get(index).setMessage(note.getMessage());;
+		}
 		return "redirect:/notes/all";
 	}
 }
